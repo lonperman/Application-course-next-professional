cb2998dfb1ce199bc0daea3a641a5dc6ba432e09
lonperman <lonperman@gmail.com>
2023-01-14 19:28:43 -0500

    Integration of PostCSS and TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      ...colors,
+    },
+  },
+};
